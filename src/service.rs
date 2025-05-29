967a2d2265a86f63221b81bc7981a57548f3cc2b
ronan <ronan_mccabe@protonmail.com>
2025-05-28 19:02:29 -0600

    Add compile tool, package caching

--- src/service.rs
+++ src/service.rs
@@ -1,14 +1,21 @@
-use anyhow::Result;
 use rmcp::{model::*, service::RequestContext, tool, Error as McpError, RoleServer, ServerHandler};
 use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[derive(Clone)]
-pub struct ElmService;
+pub struct ElmService {
+    packages: Arc<Mutex<Option<serde_json::Value>>>,
+}
+
+const ELM_PROJECT_FOLDER: Option<&str> = std::option_env!("ELM_PROJECT_FOLDER");
 
 #[tool(tool_box)]
 impl ElmService {
     pub fn new() -> Self {
-        Self
+        Self {
+            packages: Default::default(),
+        }
     }
 
     #[tool(description = "Gets the latest available package version for <USERNAME>/<PACKAGE>")]
@@ -22,15 +29,15 @@ impl ElmService {
             username, package
         ))
         .await
-        .unwrap()
+        .map_err(|e| McpError::internal_error(format!("Package fetch fail: {}", e), None))?
         .json()
         .await
-        .unwrap();
+        .map_err(|e| McpError::internal_error(format!("Package decode fail: {}", e), None))?;
         let latest_version = releases
             .iter()
             .max_by_key(|&(_, timestamp)| timestamp)
             .map(|(version, _)| version)
-            .unwrap();
+            .ok_or(McpError::internal_error("Package list empty", None))?;
         Ok(CallToolResult::success(vec![Content::text(
             latest_version.to_string(),
         )]))
@@ -48,24 +55,72 @@ impl ElmService {
             username, package, version
         ))
         .await
-        .unwrap()
+        .map_err(|e| McpError::internal_error(format!("Package fetch fail: {}", e), None))?
         .json()
         .await
-        .unwrap();
-        let out = Content::json(docs).unwrap();
+        .map_err(|e| McpError::internal_error(format!("Package decode fail: {}", e), None))?;
+        let out = Content::json(docs)?;
         Ok(CallToolResult::success(vec![out]))
     }
 
     #[tool(description = "Gets all available Elm packages")]
     async fn get_packages(&self) -> Result<CallToolResult, McpError> {
-        let docs: serde_json::Value = reqwest::get("https://package.elm-lang.org/search.json")
-            .await
-            .unwrap()
-            .json()
-            .await
-            .unwrap();
-        let out = Content::json(docs).unwrap();
+        let mut lock = self.packages.lock().await;
+        let data = match lock.clone() {
+            Some(cache) => cache,
+            None => {
+                let data: serde_json::Value =
+                    reqwest::get("https://package.elm-lang.org/search.json")
+                        .await
+                        .map_err(|e| {
+                            McpError::internal_error(format!("Packages fetch fail: {}", e), None)
+                        })?
+                        .json()
+                        .await
+                        .map_err(|e| {
+                            McpError::internal_error(format!("Packages decode fail: {}", e), None)
+                        })?;
+                *lock = Some(data.clone());
+                data
+            }
+        };
+        let out = Content::json(data)?;
         Ok(CallToolResult::success(vec![out]))
+    }
+
+    #[tool(description = "Compiles and validates the current Elm project")]
+    async fn validate(&self) -> Result<CallToolResult, McpError> {
+        let Some(path) = ELM_PROJECT_FOLDER else {
+            return Err(McpError::internal_error("Missing project path", None));
+        };
+        let output = std::process::Command::new("elm")
+            .arg("make")
+            .arg("--output=/dev/null")
+            .arg("--report=json")
+            .arg("./src/Main.elm")
+            .current_dir(&path)
+            .output()
+            .map_err(|e| {
+                McpError::internal_error(format!("Failed to run Elm compiler: {}", e), None)
+            })?;
+
+        let err = String::from_utf8_lossy(&output.stderr);
+
+        if err.is_empty() {
+            Ok(CallToolResult::success(vec![Content::text(
+                "OK".to_string(),
+            )]))
+        } else {
+            let err_data: serde_json::Value =
+                serde_json::from_str(&err)
+                    .ok()
+                    .ok_or(McpError::internal_error(
+                        "Compile error serialize fail",
+                        None,
+                    ))?;
+            let out = Content::json(err_data)?;
+            Ok(CallToolResult::success(vec![out]))
+        }
     }
 }
 
